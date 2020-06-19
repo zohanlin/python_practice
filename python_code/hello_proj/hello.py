8b01395328d3dad15bc332eb2ee227ecfdc0f2e0
zohanlin <sm9195@gmail.com>
2020-06-19 16:21:11 +0800

    Add files via upload
    Hello World & import test

---
+++ python_code/hello_proj/hello.py
@@ -0,0 +1,4 @@
+print('Hello World!')
+name = input('請輸入世界的名稱 : ')
+print('Hello 啊', name, '!')
+print('GoGO!')
---
+++ python_code/test_01.py
@@ -0,0 +1,6 @@
+﻿#import hello_proj.hello as hello
+#from hello_proj import hello
+from hello_proj.hello import name
+
+#print('再選哪個世界？ ', hello.name, '!!!')
+print('再選哪個世界？ ', name, '!!!')
